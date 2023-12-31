7d3b1eac3b9ad03ca60f18e1b90122f40c4f3d17
618knot <publicac.clt618knot@gmail.com>
2024-01-01 01:04:04 +0900

    nullチェック

--- lib/utility/move_to_url.dart
+++ lib/utility/move_to_url.dart
@@ -1,7 +1,8 @@
 import 'package:url_launcher/url_launcher.dart';
 
 void moveToUrl({String? url}) async {
-  Uri uri = Uri.parse(url!);
+  if (url == null) throw 'URL cannot be null';
+  Uri uri = Uri.parse(url);
   if (await canLaunchUrl(uri)) {
     await launchUrl(uri);
   } else {
