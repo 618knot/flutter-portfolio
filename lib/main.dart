3efda4eb2abb0135906265ceeb443c745e1da22b
618knot <publicac.clt618knot@gmail.com>
2023-12-31 15:52:51 +0900

    ダークモード実装

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,21 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isDarkMode = true;
+
+  void _changeDarkMode() {
+    setState(() {
+      _isDarkMode = !_isDarkMode;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,70 +27,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           colorSchemeSeed: const Color.fromARGB(255, 63, 101, 234),
           useMaterial3: true,
-          brightness: Brightness.light),
-      darkTheme: ThemeData(
-          colorSchemeSeed: const Color.fromARGB(255, 63, 101, 234),
-          useMaterial3: true,
-          brightness: Brightness.dark),
-      home: const MyHomePage(),
+          brightness: _isDarkMode ? Brightness.light : Brightness.dark),
+      home: MyHomePage(changeDarkMode: _changeDarkMode),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  final VoidCallback changeDarkMode;
+  const MyHomePage({Key? key, required this.changeDarkMode}) : super(key: key);
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: MyAppBar(),
+    return Scaffold(
+      appBar: const MyAppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: widget.changeDarkMode,
+        child: const Icon(Icons.light_mode),
+      ),
     );
   }
 }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.titleLarge,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ),
-//     );
-//   }
-// }
