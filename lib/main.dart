3c7b3c981a704c8cb983d9149cfcaa2d21477d82
618knot <publicac.clt618knot@gmail.com>
2023-12-31 18:52:04 +0900

    ダークモード状態のゲッターを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_portfolio/pages/about_page.dart';
 import 'components/my_app_bar.dart';
 
 void main() {
@@ -20,6 +21,11 @@ class _MyAppState extends State<MyApp> {
       _isDarkMode = !_isDarkMode;
     });
   }
+
+  bool _getIsDarkMode() {
+    return _isDarkMode;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -28,14 +34,15 @@ class _MyAppState extends State<MyApp> {
           colorSchemeSeed: const Color.fromARGB(255, 63, 101, 234),
           useMaterial3: true,
           brightness: _isDarkMode ? Brightness.dark : Brightness.light),
-      home: MyHomePage(changeDarkMode: _changeDarkMode),
+      home: MyHomePage(changeDarkMode: _changeDarkMode, isDarkMode: _getIsDarkMode),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   final VoidCallback changeDarkMode;
-  const MyHomePage({Key? key, required this.changeDarkMode}) : super(key: key);
+  final bool Function() isDarkMode;
+  const MyHomePage({Key? key, required this.changeDarkMode, required this.isDarkMode}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
